d235fe2336d4f0faf7ae78a6049b410212a0b507
jeonhl7579 <tpgus19@gmail.com>
2021-07-20 20:33:56 +0900

    [ADD]BOJ1652(해결)

---
+++ BFS/BOJ2178.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+miro=[]
+for i in range(N):
+    miro.append(list(map(int,input().rstrip())))
+
+#print(miro)
+def bfs(x,y):
+    q=deque()
+    q.append((x,y))
+
+    dx=[-1,1,0,0]
+    dy=[0,0,1,-1]
+
+    while q:
+        a,b=q.popleft()
+
+        for i in range(4):
+            na=a+dx[i]
+            nb=b+dy[i]
+
+            if 0<=na and na<N and 0<=nb and nb<M:
+                if miro[na][nb] == 1:
+                    miro[na][nb]=miro[na][nb]+miro[a][b]
+                    q.append((na,nb))
+for i in range(N):
+    for j in range(M):
+        if miro[i][j]==1:
+            bfs(i,j)
+print(miro[N-1][M-1])
+print(miro)
---
+++ SDS기본반/BOJ1652.py
@@ -0,0 +1,42 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+
+room=[]
+for _ in range(N):
+    room.append(list(map(str,input().rstrip())))
+
+print(room)
+#가로
+row=0
+#세로
+col=0
+
+for i in range(N):
+    cnt=0
+    for j in range(N):
+        if room[i][j] == 'X':
+            if cnt >= 2:
+                row+=1
+            cnt=0
+        else:
+            cnt+=1
+    if cnt >= 2:
+        row+=1
+
+for i in range(N):
+    cnt=0
+    for j in range(N):
+        if room[j][i] == 'X':
+            if cnt >= 2:
+                col+=1
+            cnt=0
+        else:
+            cnt+=1
+    if cnt >= 2:
+        col+=1
+
+print(row,end=' ')
+print(col)
+        
